64615613ce556f064344d81fd151a99c664ce583
liujingyu <jingyu525@gmail.com>
2017-10-31 11:07:07 +0800

    feat: add end_point param into config/sms.php

--- config/sms.php
+++ config/sms.php
@@ -10,6 +10,7 @@ return [
             'client_id' => env('ALIYUN_SMS_CLIENT_ID', null),
             'client_secret' => env('ALIYUN_SMS_CLIENT_SECRET', null),
             'sign_name' => env('ALIYUN_SMS_SIGN_NAME', null),
+            'end_point' => env('ALIYUN_SMS_END_POINT', null),
             'template_codes' => [
                 'SMS_1234' => [
                     'name' => '测试验证码',
